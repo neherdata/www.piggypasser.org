8923b7bce19fa723c17f2597734134967c9b2f5e
Tyler M. Neher <tyler@neherdata.com>
2024-12-25 21:30:51 -0500

    combined
    Signed-off-by: Tyler M. Neher <tyler@neherdata.com>

--- addPlayers.js
+++ addPlayers.js
@@ -4,35 +4,35 @@ let form = document.getElementById("form");
 let playerList = [];
 
 function addInput() {
-  counter++;
-  let input = document.createElement("input");
-  input.placeholder = "Player name";
-  input.id = "playerInput" + counter;
-  input.className = "player-input";
-  input.addEventListener("keydown", (event) => {
-    // when a keyDown event is detected
-    if (event.key == "Enter") {
-      // and IF that key is 'Enter'
-      event.preventDefault(); // prevent default page reload
-      addInput(); // run addInput again to add an input below
-    }
-  });
-  form.appendChild(input);
-  input.focus();
-  document.getElementById("start").disabled = false;
+    counter++;
+    let input = document.createElement("input");
+    input.placeholder = "Player name";
+    input.id = "playerInput" + counter;
+    input.className = "player-input";
+    input.addEventListener("keydown", (event) => {
+        // when a keyDown event is detected
+        if (event.key == "Enter") {
+            // and IF that key is 'Enter'
+            event.preventDefault(); // prevent default page reload
+            addInput(); // run addInput again to add an input below
+        }
+    });
+    form.appendChild(input);
+    input.focus();
+    document.getElementById("start").disabled = false;
 }
 
 function startGame() {
-  let list = form.children;
-  for (let i = 0; i < list.length; i++) {
-    let input = document.getElementById(list[i].id).value;
-    playerList.push(input);
-  }
+    let list = form.children;
+    for (let i = 0; i < list.length; i++) {
+        let input = document.getElementById(list[i].id).value;
+        playerList.push(input);
+    }
 }
 
 function go() {
-  let params = new URLSearchParams();
-  params.append("playerList", JSON.stringify(playerList));
-  let url = "game.html?" + params.toString();
-  location.replace(url);
+    let params = new URLSearchParams();
+    params.append("playerList", JSON.stringify(playerList));
+    let url = "game.html?" + params.toString();
+    location.replace(url);
 }
